0a02f47a9b325d01f9df0cd2ada6d370bfabc75c
Wens Jonás Alonso Lanza <jonasalonsolanza@gmail.com>
2020-05-05 22:34:14 +0200

    first commit

---
+++ src/test/java/com/business/keeper/exchangerateservice/ExchangeRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.keeper.exchangerateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
